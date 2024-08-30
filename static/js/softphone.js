b4132850e54b17a7a2bc7420d600e12b633d6566
RafaelZelak <rzf1503@gmail.com>
2024-08-30 14:16:47 -0300

    Lista modificada

--- static/js/softphone.js
+++ static/js/softphone.js
@@ -3,10 +3,18 @@ document.getElementById('openSidebar').addEventListener('click', function() {
     sidebar.classList.toggle('sidebar-open');
     sidebar.classList.toggle('sidebar-closed');
     const content = document.getElementById('sidebarContent');
+    const listicon = document.getElementById('list-icon');
+
     if (sidebar.classList.contains('sidebar-open')) {
         content.classList.remove('hidden-scroll');
+        if (listicon) {
+            listicon.src = 'static/img/listActive.svg'; // Troca para o ícone ativo
+        }
     } else {
         setTimeout(() => content.classList.add('hidden-scroll'), 300); // Esperar animação da sidebar
+        if (listicon) {
+            listicon.src = 'static/img/list.svg'; // Troca para o ícone desativado
+        }
     }
 });
 
@@ -23,4 +31,3 @@ document.getElementById('searchBox').addEventListener('input', function() {
         }
     });
 });
-
